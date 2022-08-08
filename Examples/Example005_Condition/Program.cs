e1fc2fcf35abbe6cafafa9f2fcfe8e463416c162
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-08 16:16:15 +0300

    save

--- Examples/Example005_Condition/Program.cs
+++ Examples/Example005_Condition/Program.cs
@@ -1,12 +1,15 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+﻿
 
-if(username.ToLower() == "полина")
-{
-    Console.WriteLine("Ура, это же ПОЛИНА!");
-}
-else
-{
+ Console.Write("Введите имя пользователя: ");
+  string username = Console.ReadLine();
+
+  if (username.ToLower() == "полина")
+ {
+                      Console.WriteLine("Ура, это же ПОЛИНА!");
+ }
+ else
+ {
     Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+        Console.WriteLine(username);
+  }
+    
