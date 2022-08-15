738cb3e2e8ba7921f0128b87430a47356c9cc543
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2022-08-15 19:58:48 +0300

    Example 12

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -38,18 +38,105 @@ int year = Method3();
 //Console.WriteLine(year);
 
 //Вид 4
+// string Method4(int count, string text)  
+// {
+//     int i = 0;
+//     string result = ""; //пустая строка - ""
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)  
 {
-    int i = 0;
     string result = ""; //пустая строка - ""
-
-    while (i < count)
+    for(int i = 0; i < count; i++) //for - все собирает; счетчик
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, "qwer");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+//Вывод таблицы умножения
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10 ; j++)
+    {
+       Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+//===== Работа с текстом
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие ьуквы "к" заменить большими "К",
+//а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаяСь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// sttring s = "qwr"
+//              012
+// s[2] // r
+/*
+string Replace(string text, char oldValue, char newValue ); //char - конкретный символ Value
+{
+    string result = String.Empty; //пустая строка
+    
+    int lenght = text.Lenght; //кол-во символов в строке
+    for (int i = 0; i < length; i++)
+    {
+       if(text[i] == oldValue) result = result + $"{newValue}";
+       else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ','|');
+
+Console.WriteLine(newText);
+*/
+
+int[] arr ={ 1, 2, 6, 7, 5, 3, 4, 3, 1};
+
+void PrintArray(int[] array)
+{
+   int count = array.Length;
+
+   for (int i = 0; i < count; i++)
+   {
+       Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    
+    for (int i = 0; i < array.Length -1 ; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
